d625ecb0843af5321711681a40e797121da129e5
CV17d <cristian.velasco@campusucc.edu.co>
2026-04-28 21:05:51 -0500

    fix: limitar tamaño del icono SVG del logo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
 
       {/* --- LOGO SUPERIOR --- */}
       <div className="login-brand-header">
-        <svg className="brand-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="brand-icon" width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M13 2L3 14H12L11 22L21 10H12L13 2Z" fill="currentColor" />
         </svg>
         <span className="brand-text">GridMind</span>
