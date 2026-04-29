49c24a29064d68697e618ac567bc7a921fc86acb
CV17d <cristian.velasco@campusucc.edu.co>
2026-04-28 21:15:23 -0500

    fix: restaurar diseño cinético original de Login, Registro y Password

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -51,7 +51,7 @@ export default function RegisterPage() {
 
       {/* --- LOGO SUPERIOR --- */}
       <div className="login-brand-header">
-        <svg className="brand-icon" width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="brand-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M13 2L3 14H12L11 22L21 10H12L13 2Z" fill="currentColor" />
         </svg>
         <span className="brand-text">GridMind</span>
